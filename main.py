a75ccb06f48a8c377138d4450be3e3f186d67c0a
Michal <michalzajdel2002@gmail.com>
2024-04-12 15:21:08 +0200

    pixel_map

--- main.py
+++ main.py
@@ -1,2 +1,59 @@
+import tkinter as tk
+import threading
+import time
+import random as rnd
 
-print("Hello, World!")
+class PixelMap :
+    def __init__(self, master, width, height, pixel_size, map):
+        self.master = master
+        self.width = width
+        self.height = height
+        self.pixel_size = pixel_size
+        self.map = map
+
+        self.canvas = tk.Canvas(master, width=width*pixel_size, height=height*pixel_size)
+        self.canvas.pack()
+
+        self.draw_map()
+
+    def draw_map(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                if self.map[i][j] == 'W':
+                    color = "black"
+                elif self.map[i][j] == 'F':
+                    color = "grey"
+
+                self.canvas.create_rectangle(
+                    i*self.pixel_size,
+                    j*self.pixel_size,
+                    (i+1)*self.pixel_size,
+                    (j+1)*self.pixel_size,
+                    fill=color
+                )
+
+def read_map(filename):
+
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            map = []
+            for line in lines:
+                line_to_chars = [char for char in line if char != '\n']
+                map.append(line_to_chars)
+            return map
+    except FileNotFoundError:
+        print("Nie znaleziono pliku o podanej nazwie.")
+        return None
+    
+def main():
+    root = tk.Tk()
+    root.title("Pixel Map")
+
+    map = read_map("base_map.txt")
+    if map is not None:
+        pixel_map = PixelMap(master=root, height=len(map), width=len(map[0]), pixel_size=4, map=map)
+        root.mainloop()
+
+if __name__ == "__main__":
+    main()
