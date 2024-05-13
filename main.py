2ecb9cc51672995c96721e40f38b4906027c30c3
kvpe <jakub.kolbertowicz@gmail.com>
2024-05-13 18:25:53 +0200

    handled critical section - only 1 player per square

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import tkinter as tk
 import threading
 import time
 import random as rnd
-from pynput import keyboard
 
-class PixelMap :
+class PixelMap:
     def __init__(self, master, width, height, pixel_size, map):
         self.master = master
         self.width = width
@@ -14,20 +13,29 @@ class PixelMap :
 
         self.isRunning = True
 
-        self.players = []
-
-        self.move_players_threads = []
-
-        self.human_player = [rnd.randint(3, self.width-4), rnd.randint(3, self.height-4)]
-        new_thread = threading.Thread(target=self.start_press_listener)
-        self.move_players_threads.append(new_thread)
-        self.move_players_threads[0].start()
+        self.players = set()
+        self.players_lock = threading.Lock()
+        self.canvas_lock = threading.Lock()
+        self.position_locks = {} #slownik
 
         self.canvas = tk.Canvas(master, width=width*pixel_size, height=height*pixel_size)
         self.canvas.pack()
 
         self.draw_map()
-        self.move_player(self.human_player, -1)
+
+        self.human_player1 = [rnd.randint(3, self.width-4), rnd.randint(3, self.height-4)]
+        self.human_player2 = [rnd.randint(3, self.width-4), rnd.randint(3, self.height-4)]
+
+        self.move_player_thread1 = threading.Thread(target=self.start_press_listener1)
+        self.move_player_thread2 = threading.Thread(target=self.start_press_listener2)
+        self.move_player_thread1.start()
+        self.move_player_thread2.start()
+        self.move_player(self.human_player1[0], self.human_player1[1], -1)
+        self.move_player(self.human_player2[0], self.human_player2[1], -1)
+
+        self.ai_player = [rnd.randint(3, self.width-4), rnd.randint(3, self.height-4)]
+        self.move_ai_thread = threading.Thread(target=self.move_ai)
+        self.move_ai_thread.start()
 
     def draw_map(self):
         for i in range(self.width):
@@ -44,83 +52,106 @@ class PixelMap :
                     (j+1)*self.pixel_size,
                     fill=color
                 )
-        
-    def add_player_AI(self):
-        new_player = [rnd.randint(3, self.width-4), rnd.randint(3, self.height-4)]
-        self.players.append(new_player)
-        index = len(self.move_players_threads)
-        new_thread = threading.Thread(target=self.move_player_index, args=(index-1,))
-        self.move_players_threads.append(new_thread)
-        self.move_players_threads[index].start()
-        
+                self.position_locks[f"{i}_{j}"] = threading.Lock()
 
-    def move_player(self, player, direction):
-        # 0 - up, 1 - right, 2 - down, 3 - left
+    def move_player(self, x, y, direction):
+        new_position = [x, y]
         if direction != -1:
-            self.canvas.create_rectangle(
-                    player[0]*self.pixel_size,
-                    player[1]*self.pixel_size,
-                    (player[0]+1)*self.pixel_size,
-                    (player[1]+1)*self.pixel_size,
-                    fill="grey"
-                )
-            if direction == 0:
-                if player[1] > 3:
-                    player[1] -= 1
-            elif direction == 1:
-                if player[0] < self.width-3:
-                    player[0] += 1
-            elif direction == 2:
-                if player[1] < self.height-4:
-                    player[1] += 1
-            elif direction == 3:
-                if player[0] > 2:
-                    player[0] -= 1
-        self.canvas.create_rectangle(
-            player[0]*self.pixel_size,
-            player[1]*self.pixel_size,
-            (player[0]+1)*self.pixel_size,
-            (player[1]+1)*self.pixel_size,
-            fill="red"
-        )
+            if direction == 0 and y > 3:
+                new_position[1] -= 1
+            elif direction == 1 and x < self.width-3:
+                new_position[0] += 1
+            elif direction == 2 and y < self.height-4:
+                new_position[1] += 1
+            elif direction == 3 and x > 2:
+                new_position[0] -= 1
 
-    def move_player_index(self,index):
-        while self.isRunning:
-            time.sleep(0.1)
-            self.move_player(self.players[index], rnd.randint(0, 3))
-            time.sleep(0.1)
+        target_position = f"{new_position[0]}_{new_position[1]}"
+        
+        with self.position_locks[target_position]:
+            with self.players_lock:
+                if target_position not in self.players:
+                    self.players.discard(f"{x}_{y}")
+                    self.players.add(target_position)
+                    with self.canvas_lock:
+                        self.canvas.create_rectangle(
+                            x*self.pixel_size,
+                            y*self.pixel_size,
+                            (x+1)*self.pixel_size,
+                            (y+1)*self.pixel_size,
+                            fill="grey"
+                        )
+                        if [x, y] == self.human_player1:
+                            color = "red"
+                        elif [x, y] == self.human_player2:
+                            color = "blue"
+                        else:
+                            color = "green"
+                        self.canvas.create_rectangle(
+                            new_position[0]*self.pixel_size,
+                            new_position[1]*self.pixel_size,
+                            (new_position[0]+1)*self.pixel_size,
+                            (new_position[1]+1)*self.pixel_size,
+                            fill=color
+                        )
+                    return new_position
+        return [x, y]
 
-
-    def on_press(self, key):
+    def on_press1(self, event):
         try:
             direction = -1
-            if key == keyboard.Key.up:
+            if event.keysym == 'Up':
                 direction = 0
-            elif key == keyboard.Key.right:
+            elif event.keysym == 'Right':
                 direction = 1
-            elif key == keyboard.Key.down:
+            elif event.keysym == 'Down':
                 direction = 2
-            elif key == keyboard.Key.left:
+            elif event.keysym == 'Left':
                 direction = 3
-            self.move_player(self.human_player, direction)
+            self.human_player1 = self.move_player(self.human_player1[0], self.human_player1[1], direction)
         except AttributeError:
             pass
 
-    def on_release(self, key):
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
-    
-    def start_press_listener(self):
-        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
-            listener.join()
+    def on_press2(self, event):
+        try:
+            direction = -1
+            if event.keysym == 'w':
+                direction = 0
+            elif event.keysym == 'd':
+                direction = 1
+            elif event.keysym == 's':
+                direction = 2
+            elif event.keysym == 'a':
+                direction = 3
+            self.human_player2 = self.move_player(self.human_player2[0], self.human_player2[1], direction)
+        except AttributeError:
+            pass
+
+    def start_press_listener1(self):
+        self.master.bind("<Up>", self.on_press1)
+        self.master.bind("<Down>", self.on_press1)
+        self.master.bind("<Left>", self.on_press1)
+        self.master.bind("<Right>", self.on_press1)
+
+    def start_press_listener2(self):
+        self.master.bind("<w>", self.on_press2)
+        self.master.bind("<s>", self.on_press2)
+        self.master.bind("<a>", self.on_press2)
+        self.master.bind("<d>", self.on_press2)
+
+    def move_ai(self):
+        while self.isRunning:
+            direction = rnd.randint(0, 3)
+            self.ai_player = self.move_player(self.ai_player[0], self.ai_player[1], direction)
+            time.sleep(0.5)
 
     def close(self):
         self.isRunning = False
-        self.move_players_threads[0]._delete()
+        self.move_player_thread1.join()
+        self.move_player_thread2.join()
+        self.move_ai_thread.join()
 
 def read_map(filename):
-
     try:
         with open(filename, 'r') as file:
             lines = file.readlines()
@@ -139,19 +170,12 @@ def on_closing(map):
 
 def main():
     root = tk.Tk()
-    
     root.title("Pixel Map")
-
-
     map = read_map("base_map.txt")
     if map is not None:
         pixel_map = PixelMap(master=root, height=len(map), width=len(map[0]), pixel_size=6, map=map)
-        time.sleep(1)
-        pixel_map.add_player_AI()
-        time.sleep(1)
-        pixel_map.add_player_AI()
+        root.protocol("WM_DELETE_WINDOW", lambda: on_closing(pixel_map))
         root.mainloop()
-
 
 if __name__ == "__main__":
     main()
